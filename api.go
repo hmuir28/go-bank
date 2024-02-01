19500531d54544a2a23c97af4ab95be52c77cae0
Harry Muir Camacho <hmuir@Harrys-MacBook-Pro.local>
2024-01-31 21:33:39 -0600

    Added server to run

--- api.go
+++ api.go
@@ -1,6 +1,33 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func writeJSON(w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
+
+	return json.NewEncoder(w).Encode(v)
+}
+
+type APIFunc func(http.ResponseWriter, *http.Request) error
+
+type APIError struct {
+	Error string
+}
+
+func makeHttpHandleFunc(f APIFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			writeJSON(w, http.StatusBadRequest, APIError{Error: err.Error()})
+		}
+	}
+}
 
 type APIServer struct {
 	listenAddr string
@@ -10,6 +37,16 @@ func NewAPIServer(listenAddr string) *APIServer {
 	return &APIServer{
 		listenAddr: listenAddr,
 	}
+}
+
+func (s *APIServer) Run() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/account", makeHttpHandleFunc(s.handleAccount))
+
+	log.Println("JSON API server running on port: ", s.listenAddr)
+
+	http.ListenAndServe(s.listenAddr, router)
 }
 
 func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/hmuir28/go-bank
 
 go 1.20
+
+require github.com/gorilla/mux v1.8.1 // indirect
